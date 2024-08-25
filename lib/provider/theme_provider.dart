8f678080bda51399ffb99f1002d83d8440f55f11
rifftyo <rifftyo@gmail.com>
2024-08-25 11:53:12 +0700

    Add Theme Provider

---
+++ lib/provider/theme_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  bool _isDark = false;
+
+  bool get isDark => _isDark;
+
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  void _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDark = prefs.getBool('isDark') ?? false;
+    notifyListeners();
+  }
+
+  void toggleTheme() async {
+    _isDark = !_isDark;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDark', _isDark);
+    notifyListeners();
+  }
+}
