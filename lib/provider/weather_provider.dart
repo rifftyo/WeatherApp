2e2dcc5cb3fed9d5b1fc387f86287833a7abb9fa
rifftyo <rifftyo@gmail.com>
2024-08-25 10:58:04 +0700

    Add Weather Provider

---
+++ lib/provider/weather_provider.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/data/api/api_service.dart';
+import 'package:weather_app/data/model/weather_results.dart';
+
+enum ResultState { loading, noData, hasData, error }
+
+class WeatherProvider extends ChangeNotifier {
+  final ApiService apiService;
+
+  WeatherProvider({required this.apiService}) {
+    _fetchWeather();
+  }
+
+  late WeatherResults _weatherResults;
+  late ResultState _state;
+  String _message = '';
+
+  String get message => _message;
+
+  WeatherResults get result => _weatherResults;
+
+  ResultState get state => _state;
+
+  Future<dynamic> _fetchWeather() async {
+    try {
+      _state = ResultState.loading;
+      notifyListeners();
+      final weather = await apiService.getWeather('Sleman');
+      if (weather.name.isEmpty) {
+        _state = ResultState.noData;
+        notifyListeners();
+        return _message = 'Empty Data';
+      } else {
+        _state = ResultState.hasData;
+        notifyListeners();
+        return _weatherResults = weather;
+      }
+    } catch (e) {
+      _state = ResultState.error;
+      notifyListeners();
+      return _message = 'Error --> $e';
+    }
+  }
+}
