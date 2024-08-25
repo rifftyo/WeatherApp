ac03e8e71be1c2c2310629963e26f9c980859d09
rifftyo <rifftyo@gmail.com>
2024-08-25 11:53:41 +0700

    Add Provider Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:weather_app/data/api/api_service.dart';
-import 'package:weather_app/provider/weather_provider.dart';
+import 'package:weather_app/provider/theme_provider.dart';
 import 'package:weather_app/ui/home_page.dart';
 
 void main() {
   runApp(
     ChangeNotifierProvider(
-      create: (context) => WeatherProvider(apiService: ApiService()),
+      create: (_) => ThemeProvider(),
       child: const MyApp(),
     ),
   );
@@ -18,10 +17,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Weather App',
-      home: HomePage(),
+    return Consumer<ThemeProvider>(
+      builder: (context, theme, _) {
+        return MaterialApp(
+          theme: theme.isDark ? ThemeData.dark() : ThemeData.light(),
+          debugShowCheckedModeBanner: false,
+          title: 'Weather App',
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
